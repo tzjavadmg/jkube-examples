32fd65bb7c86c39fb524d30d59580f891dd1a4a6
codyzeng <tzjavadmg@163.com>
2022-10-29 23:07:48 +0800

    热部署示例

--- hot-deployment/src/main/java/com/codyzeng/jkube/HotDeploymentController.java
+++ hot-deployment/src/main/java/com/codyzeng/jkube/HotDeploymentController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class HotDeploymentController {
     @RequestMapping("/")
     public String index() {
-        return "k8s中的spring boot服务,使用k8s:watch实现热部署！";
+        return "使用k8s:watch实现热部署！";
     }
 
-    @RequestMapping("/newMethod")
-    public String newMethod() {
-        return "新增一个方法";
-    }
+//    @RequestMapping("/newMethod")
+//    public String newMethod() {
+//        return "新增一个方法";
+//    }
 
 }
