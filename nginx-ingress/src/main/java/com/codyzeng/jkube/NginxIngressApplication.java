d9779899fe8a9c41edba67d12db4ff1f85e72147
codyzeng <tzjavadmg@163.com>
2022-10-31 19:57:50 +0800

    nginx Ingress Controller

---
+++ nginx-ingress/src/main/java/com/codyzeng/jkube/NginxIngressApplication.java
@@ -0,0 +1,16 @@
+package com.codyzeng.jkube;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: codyzeng@163.com
+ * @date: 2022/10/31
+ */
+@SpringBootApplication
+public class NginxIngressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NginxIngressApplication.class);
+    }
+
+}
